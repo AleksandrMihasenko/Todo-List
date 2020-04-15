2e7eb62e54d7488d2b153a6df5daf343580a8206
AleksandrMihasenko <aleksandr.mihasenko@gmail.com>
2020-04-15 13:45:05 +0300

    added section code that deletes todos

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const deleteTodo = document.querySelector('.list-todos');
+
+
+//delete todos event
+deleteTodo.addEventListener('click', event => {
+
+    if (event.target.classList.contains('icon-delete')) {
+        event.target.parentElement.remove();
+    }
+
+});
 
