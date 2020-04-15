32cc89684ad74822373e16eeab20c31a2f8df919
AleksandrMihasenko <aleksandr.mihasenko@gmail.com>
2020-04-15 15:02:23 +0300

    added section code which adds todos

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const deleteTodo = document.querySelector('.list-todos');
+const list = document.querySelector('.list-todos');
+const addTodo = document.querySelector('.add');
+const search = document.querySelector('.form-search');
+
+const generateTemplate = todo => {
+    const html = `
+        <li class="list-todos-item">
+            <span>${todo}</span>
+            <img src="/img/trash-bin.svg" class="icon-delete" alt="trash-icon">                
+        </li>
+    `;
+    list.innerHTML += html;
+};
 
 
 //delete todos event
-deleteTodo.addEventListener('click', event => {
+list.addEventListener('click', event => {
 
     if (event.target.classList.contains('icon-delete')) {
         event.target.parentElement.remove();
@@ -10,3 +22,17 @@ deleteTodo.addEventListener('click', event => {
 
 });
 
+
+//add todos event
+addTodo.addEventListener('submit', event => {
+    
+    event.preventDefault();
+
+    const todo = addTodo.add.value.trim();
+
+    if (todo.length) {
+        generateTemplate(todo);
+        addTodo.reset();
+    }
+
+});
